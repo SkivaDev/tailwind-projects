661e302c411c01b1f9fa8f769b6fea3486ef5ff0
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-01-30 23:03:02 -0500

    a label with tailwind

--- tailwind-header/src/App.jsx
+++ tailwind-header/src/App.jsx
@@ -6,9 +6,15 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
+  <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+    <div class="shrink-0">
+      <img class="h-12 w-12" src="/src/assets/react.svg" alt="ChitChat Logo"/>
+    </div>
+    <div>
+      <div class="text-xl font-medium text-red-600">ChitChat</div>
+      <p class="text-slate-500">You have a new message!</p>
+    </div>
+  </div>
   )
 }
 
