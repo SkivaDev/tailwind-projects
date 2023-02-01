b9fda41412e93bece7b637587ac5dca6dcb6ffbb
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-01-31 22:19:23 -0500

    test 2

--- tailwind-header/src/App.jsx
+++ tailwind-header/src/App.jsx
@@ -1,28 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className='w-full h-14 bg-red-500'>
-      <div className='max-w-full h-14 flex justify-between px-10 items-center'>
-        <div className='w-8 h-8'>
-          <img className='w-full h-full' src="./assets/react.svg" alt="logo" />
-        </div>
-        <div className='flex gap-6 items-center'>
-          <button className='hover:text-white hover:cursor-pointer text-xl'>
-            Home
-          </button>
-          <button className='hover:text-white hover:cursor-pointer text-xl'>
-            Main
-          </button>
-          <button className='hover:text-white hover:cursor-pointer text-xl'>
-            Hello
-          </button>
+    <div className="h-screen">
+      <div className="relative h-4/5 block">
+        <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-sky-500 to-indigo-500 -skew-y-12 overflow-hidden origin-top-left -z-10"></div>
+        <div className="">
+          <div className="">
+            <h1>The best app you've ever seen</h1>
+            <p>You'll be blown away with all it does</p>
+            <button>Sign up</button>
+          </div>
+          <div className="">
+            <div className=""></div>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
